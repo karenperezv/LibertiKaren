53ada9bb283b9442f8a9f2d551c541c2ef030905
Karen Perez <karenpv96726@gmail.com>
2018-09-20 21:17:54 -0500

    primer commit

---
+++ app/src/main/java/app/liberti/com/liberti/Registro_Finalizar_Cuenta.kt
@@ -0,0 +1,12 @@
+package app.liberti.com.liberti
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Registro_Finalizar_Cuenta : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro__finalizar__cuenta)
+    }
+}
